88d676642ec7d44e9708c90501c20bc06e7156c3
RickHardBR <rickhard@bol.com.br>
2022-02-04 22:09:35 -0300

    Estilição da página inicial e da página dos pokemons

--- pokedex/src/Homepage/Homepage.js
+++ pokedex/src/Homepage/Homepage.js
@@ -1,33 +1,35 @@
 import React from 'react'
 
-import { useNavigate } from 'react-router-dom';
+import { Container, Gira, DivPokebola, Button, Pokebola, Logo } from './Style'
+
+import './Home.css'
+
+import { useNavigate } from 'react-router-dom'
 
 export default function HomePage() {
-
-  const navigate = useNavigate();
+  const navigate = useNavigate()
 
   const pagePokeIndex = () => {
-    navigate("/PokeIndex");
-  };
+    navigate('/PokeIndex')
+  }
 
   const pagePokedex = () => {
-    navigate("/Pokedex");
-  };
+    navigate('/Pokedex')
+  }
 
   const pageDetails = () => {
-    navigate("/Details");
-  };
- 
-  return(
-    <div>
-      HomePage
-        <></>
-      <button onClick={pagePokeIndex}>Poke Index</button>
-      <button onClick={pagePokedex}>Poke Dex</button>
-      <button onClick={pageDetails}>Details</button>
-      <center>
-      <img height="1000px" alt="homepage" src="img/pageteste.png" />
-      </center>
-    </div>
+    navigate('/Details')
+  }
+
+  return (
+    <Container>
+      <Logo alt="homepage" src="img/logo.png" />
+      <DivPokebola>
+        <Button onClick={pagePokeIndex}>
+           <Pokebola alt="Pokebola" src="img/pokebola.png" />
+        </Button>
+        <button onClick={pagePokedex}>Poke Dex</button>
+        </DivPokebola>
+    </Container>
   )
 }
